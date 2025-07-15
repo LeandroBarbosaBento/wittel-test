334bc22966ae6238459c51e99ca7ab82d9aaabc3
leandro <leandro@pop-os.localdomain>
2025-07-15 18:19:51 -0300

    feat: create customer card

--- resources/js/components/CustomerCard.vue
+++ resources/js/components/CustomerCard.vue
@@ -1,39 +1,36 @@
 <template>
     <div class="relative">
-
-
         <div class="bg-white rounded shadow-md border border-gray-200 my-8 p-4 flex justify-between items-center" >
             <div>
                 <p class="font-semibold">{{ customer.name }}</p>
                 <p class="text-sm text-gray-600">CPF: {{ customer.cpf }}</p>
             </div>
             <span
-                class="p-1 bg-blue-100 text-blue-800 font-bold rounded-md flex items-center justify-center text-center text-sm"
+                class="min-w-20 p-1 font-semibold rounded-md flex items-center justify-center text-center text-sm"
                 :class="customerClass"
             >
-                {{ customer.household_income.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                {{
+                    customer.household_income.toLocaleString('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    })
+                }}
             </span>
         </div>
     </div>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed } from 'vue';
 
 const props = defineProps({
     customer: {
         type: Object,
         required: true
     },
-})
+});
 
 const emit = defineEmits(['update:modelValue']);
-
-const currentPage = computed(() => props.modelValue);
-
-const changePage = (page) => {
-    emit('update:modelValue', page);
-}
 
 const customerClass = computed(() => {
     if(props.customer.household_income <= 980) {
@@ -43,6 +40,6 @@ const customerClass = computed(() => {
     } else {
         return 'text-red-600 bg-red-200';
     }
-})
+});
 
 </script>
