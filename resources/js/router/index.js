a5bd5573976aa34e68143bde6f111ac982096c7e
leandro <leandro@pop-os.localdomain>
2025-07-15 20:25:46 -0300

    feat: create dashboard page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -5,6 +5,10 @@ const routes = [
     path: '/',
     component: () => import('../pages/Home.vue'),
   },
+  {
+    path: '/dashboard',
+    component: () => import('../pages/Dashboard.vue'),
+  }
 
 ];
 
