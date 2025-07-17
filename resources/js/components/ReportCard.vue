76b7e9ded4eb09775c273637e04e0d11844490fd
leandro <leandro@pop-os.localdomain>
2025-07-17 13:50:34 -0300

    feat: create dashboard page

---
+++ resources/js/components/ReportCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-white rounded-lg shadow p-4 flex justify-between items-center border border-gray-100 w-full sm:w-64">
+        <slot>
+            <div>
+                <p :class="`text-sm text-${color} pl-4 border-l-3 border-${color} font-semibold`">
+                    {{ text }}
+                </p>
+                <p :class="`text-3xl text-${color} font-bold p-3`">
+                    {{ number }}
+                </p>
+            </div>
+        </slot>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: false,
+    },
+    number: {
+        type: [ String, Number],
+        required: false,
+    },
+    color: {
+        type: String,
+        default: 'gray-400'
+    }
+})
+
+</script>
+
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-8 border-b border-gray-200 py-4 gap-2">
             <h1 class="text-3xl font-bold text-center sm:text-left">Dashboard </h1>
-            <div class="flex flex-col sm:flex-row gap-4 w-full sm:w-auto text-xl text-center font-semibold">
+            <div class="flex flex-col sm:flex-row gap-4 w-full sm:w-auto text-sm text-center font-semibold">
                 <span
                     :class="{
                         'border-b-3 border-orange-500 text-orange-500' : selectedPeriod === 'all'
@@ -38,31 +38,85 @@
             </div>
         </div>
 
-        <button
-            class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 cursor-pointer w-full sm:w-auto"
-            @click="getData"
-        >
-            GetData
-        </button>
+        <div class="flex flex-col sm:flex-row gap-5 w-full">
 
+            <report-card
+                text="Average household income"
+                :number="avgIncome"
+                color="orange-500"
+            />
+
+            <report-card
+                text="Adults with income over average"
+                :number="countCustomersOverAvgIncome"
+            />
+
+            <report-card>
+                <div class="flex flex-col">
+                    <div class="flex items-center justify-between mb-2 w-full">
+                        <div class="flex items-center">
+                            <span class="w-3 h-3 rounded-full bg-red-500 mr-2"></span>
+                            <span class="text-sm text-gray-700 mr-1">Class A: </span>
+                        </div>
+                        <span class="text-sm font-medium text-gray-800">
+                            {{ countClassACustomers }} customers
+                        </span>
+                    </div>
+
+                    <div class="flex items-center justify-between mb-2 w-full">
+                        <div class="flex items-center">
+                            <span class="w-3 h-3 rounded-full bg-yellow-500 mr-2"></span>
+                            <span class="text-sm text-gray-700 mr-1">Class B: </span>
+                        </div>
+                        <span class="text-sm font-medium text-gray-800">
+                            {{ countClassBCustomers }} customers
+                        </span>
+                    </div>
+
+                    <div class="flex items-center justify-between w-full">
+                        <div class="flex items-center">
+                            <span class="w-3 h-3 rounded-full bg-green-500 mr-2"></span>
+                            <span class="text-sm text-gray-700 mr-1">Class C: </span>
+                        </div>
+                        <span class="text-sm font-medium text-gray-800">
+                            {{ countClassCCustomers }} customers
+                        </span>
+                    </div>
+                </div>
+            </report-card>
+
+        </div>
     </div>
 </template>
 <script setup>
 import { ref, watch } from "vue";
 import { getReport } from '../services';
+import ReportCard from "../components/ReportCard.vue";
 
 const selectedPeriod = ref("all");
 
-watch(selectedPeriod, (newVal) => {
-    getData();
-});
+const avgIncome = ref(0);
+const countCustomersOverAvgIncome = ref(0);
+const countClassACustomers = ref(0);
+const countClassBCustomers = ref(0);
+const countClassCCustomers = ref(0);
 
 const getData = async () => {
     try {
         const data = await getReport(selectedPeriod.value);
-        console.log(data);
+        countCustomersOverAvgIncome.value = data.adults_above_avg_income;
+        avgIncome.value = data.avg_income;
+        countClassACustomers.value = data.class_A;
+        countClassBCustomers.value = data.class_B;
+        countClassCCustomers.value = data.class_C;
+
     } catch (error) {
         console.error(error);
     }
 }
+
+watch(selectedPeriod, () => {
+    getData();
+}, { immediate: true });
+
 </script>
